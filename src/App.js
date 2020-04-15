55f4b4a3ad4d0e0734a8ac33ce84b10075d1503b
kunal15abc <kunal15abc@gmail.com>
2020-04-15 17:26:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const users = [
+  {
+    id: 1,
+    name: `Kunal`,
+    surname: 'Bhattacharya',
+    dob: '15 May'
+  },
+  {
+    id: 2,
+    name: `Deepika`,
+    surname: 'Chandra',
+    dob: '8 June'
+  },
+];
+
+class UserPage extends React.Component {
+  render() {
+    const {
+      params: { userId },
+    } = this.props.match;
+
+    const userData = users.find(function (item) {
+      return item.id == userId;
+    })
+    return (
+      <>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong>ID: {userData.id}</strong>
+          <br />
+          <strong>FIRST NAME: {userData.name}</strong>
+          <br />
+          <strong>LAST NAME: {userData.surname}</strong>
+          <br />
+          <strong>DOB: {userData.dob}</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </>
+    );
+  }
+}
+
+class Home extends React.Component {
+  render() {
+    return(
+      <>
+      {
+        users.map(function(item){
+          return(
+            <>
+            <Link to={"/users/"+item.id}>{item.name}</Link>
+            <br />
+            </>
+          )
+        })
+      }
+      </>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Route
+            path="/home"
+            render={(props) => <Home />}
+          />
+          <Route
+            path="/users/:userId"
+            render={(props) => <UserPage {...props} />}
+          />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
